4a6d57203ceeffec7e5a413ad75002ab26c785ee
Ubuntu <ubuntu@ip-172-31-80-27.ec2.internal>
2019-12-09 11:34:45 +0000

    css and Html added for header, modal tweaked for responsive

--- assets/js/foundation.js
+++ assets/js/foundation.js
@@ -1,28 +1,40 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// This is used to get the modal
+var mexicoModal = document.getElementById("mexicoModal");
+var moscowModal = document.getElementById("moscowModal");
+var srilankaModal = document.getElementById("srilankaModal");
 
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-
+// this gets the button that opens the modal
+var mexicoBtn = document.getElementById("mexicoBtn");
+var moscowBtn = document.getElementById("moscowBtn");
+var srilankaBtn = document.getElementById("srilankaBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close-button")[0];
+var moscowSpan = document.getElementsByClassName("close-button-moscow")[0];
+var srilankaSpan = document.getElementsByClassName("close-button-sri-lanka")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+mexicoBtn.onclick = function() {
+    mexicoModal.style.display = "block";
 };
+
+moscowBtn.onclick = function() {
+    moscowModal.style.display = "block";
+};
+
+srilankaBtn.onclick = function() {
+    srilankaModal.style.display = "block";
+};
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    mexicoModal.style.display = "none";
+};
+moscowSpan.onclick = function() {
+    moscowModal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+srilankaSpan.onclick = function() {
+    srilankaModal.style.display = "none";
 };
