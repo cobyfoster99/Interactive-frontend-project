9b1f4b35b60fb29ab4585e55528ac70b9a0c2423
Ubuntu <ubuntu@ip-172-31-80-27.ec2.internal>
2019-12-12 11:53:55 +0000

    MailJS debug

--- assets/js/send-email.js
+++ assets/js/send-email.js
@@ -1,18 +1,18 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "Travel_Form", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "desired_location": contactForm.destination.value,
-        "phone_number": contactForm.phone.value,
-        "enquiry_details": contactForm.projectsummary.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+    emailjs.send("gmail", "travel", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "desired_location": contactForm.destination.value,
+            "phone_number": contactForm.phone.value,
+            "enquiry_details": contactForm.projectsummary.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
 }
