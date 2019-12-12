bb205420f7ca7c9275ea85125dbc0f0c3aefc675
Ubuntu <ubuntu@ip-172-31-80-27.ec2.internal>
2019-12-12 09:53:54 +0000

    Initial Commit - Mail JS

---
+++ assets/js/send-email.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "Travel Forms", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "desired_location": contactForm.destination.value,
+        "phone_number": contactForm.phone.value,
+        "enquiry_details": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
